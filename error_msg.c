d2a48adde7e2054f94f5ab7ae6658ace5e612131
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-10-19 15:58:28 +0100

    Fix: Memory leak

--- error_msg.c
+++ error_msg.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 14:38:03 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/10/19 15:02:31 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/10/19 15:44:08 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,23 @@ void	error_map(t_parsing *parsing)
 
 void	error_line(t_parsing *parsing)
 {
+	int	i;
+
+	i = 0;
+	if (parsing->north_path)
+		free(parsing->north_path);
+	if (parsing->south_path)
+		free(parsing->south_path);
+	if (parsing->west_path)
+		free(parsing->west_path);
+	if (parsing->east_path)
+		free(parsing->east_path);
+	if (parsing->map)
+	{
+		while (parsing->map[i] != NULL)
+			free(parsing->map[i++]);
+		free(parsing->map);
+	}
 	free(parsing);
 	printf("\033[1;31mError: \033[0m\n");
 	printf("\033[1;35mInvalid Line Format. Please check the format of the line \
