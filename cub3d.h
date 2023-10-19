9e049301b751a8a6c9f6ddeb440db8b1bd425edf
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-10-19 15:59:46 +0100

    FIx: Norm errors

--- cub3d.h
+++ cub3d.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 15:55:56 by tde-sous          #+#    #+#             */
+/*   Updated: 2023/10/19 15:59:36 by tde-sous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 # define CUB3D_H
 
@@ -37,22 +49,21 @@
 
 typedef struct s_parsing
 {
-	char	*north_path;    // Path to the north-facing texture.
-	char	*south_path;    // Path to the south-facing texture.
-	char	*west_path;     // Path to the west-facing texture.
-	char	*east_path;     // Path to the east-facing texture.
-	int		ceiling_color; // RGB color value for the ceiling.
-	int		floor_color;   // RGB color value for the floor.
-	char	**map;         // Maze map (2D array of characters).
-	int		number_lines;  // Number of lines in the map.
+	char	*north_path;
+	char	*south_path;
+	char	*west_path;
+	char	*east_path;
+	int		ceiling_color;
+	int		floor_color;
+	char	**map;
+	int		number_lines;
 }	t_parsing;
 
 typedef struct s_index
 {
-	int	texture_count;         // Counter for texture data (used during parsing).
-	int	color_count;        // Counter for color data (used during parsing).
-	int	map_index;     // Index for the current line in the maze map.
-	//int	new_line;       // Flag to indicate if a new line is encountered (used during parsing).
+	int	texture_count;
+	int	color_count;
+	int	map_index;
 }	t_index;
 
 typedef struct s_minimap
@@ -67,108 +78,105 @@ typedef struct s_minimap
 
 typedef struct s_texture_data
 {
-	void	*img;               // Pointer to the image data.
-	char	*addr;              // Pointer to the memory address of the image data.
-	int		bits_per_pixel;    // Number of bits per pixel in the image.
-	int		line_size;          // Size of each image line in bytes.
-	int		endian;             // Endianness of the image data.
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_size;
+	int		endian;
 
-	int		bpp_north;          // Bits per pixel for the north-facing texture.
-	int		l_len_north;        // Line length for the north-facing texture.
-	int		endian_north;       // Endianness for the north-facing texture.
+	int		bpp_north;
+	int		l_len_north;
+	int		endian_north;
 
-	int		bpp_south;          // Bits per pixel for the south-facing texture.
-	int		l_len_south;        // Line length for the south-facing texture.
-	int		endian_south;       // Endianness for the south-facing texture.
+	int		bpp_south;
+	int		l_len_south;
+	int		endian_south;
 
-	int		bpp_west;           // Bits per pixel for the west-facing texture.
-	int		l_len_west;         // Line length for the west-facing texture.
-	int		endian_west;        // Endianness for the west-facing texture.
+	int		bpp_west;
+	int		l_len_west;
+	int		endian_west;
 
-	int		bpp_east;           // Bits per pixel for the east-facing texture.
-	int		l_len_east;         // Line length for the east-facing texture.
-	int		endian_east;        // Endianness for the east-facing texture.
+	int		bpp_east;
+	int		l_len_east;
+	int		endian_east;
 
-	int		bpp_ceiling;        // Bits per pixel for the ceiling texture.
-	int		l_len_ceiling;      // Line length for the ceiling texture.
-	int		endian_ceiling;     // Endianness for the ceiling texture.
+	int		bpp_ceiling;
+	int		l_len_ceiling;
+	int		endian_ceiling;
 
-	char	*addr_north;        // Memory address of the north-facing texture data.
-	char	*addr_south;        // Memory address of the south-facing texture data.
-	char	*addr_west;         // Memory address of the west-facing texture data.
-	char	*addr_east;         // Memory address of the east-facing texture data.
-	char	*addr_ceiling;      // Memory address of the ceiling texture data.
+	char	*addr_north;
+	char	*addr_south;
+	char	*addr_west;
+	char	*addr_east;
+	char	*addr_ceiling;
 }	t_texture_data;
-
 
 typedef struct s_mlx
 {
-	void		*mlx;                // Handle to the mlx instance.
-	void		*window;             // Handle to the game window.
-	char		**map;               // 2D array representing the game map.
-	int			width;               // Width of the game window.
-	int			height;              // Height of the game window.
+	void		*mlx;
+	void		*window;
+	char		**map;
+	int			width;
+	int			height;
 
-	char		*north_texture_path; // Path to the north-facing texture image.
-	char		*south_texture_path; // Path to the south-facing texture image.
-	char		*east_texture_path;  // Path to the east-facing texture image.
-	char		*west_texture_path;  // Path to the west-facing texture image.
+	char		*north_texture_path;
+	char		*south_texture_path;
+	char		*east_texture_path;
+	char		*west_texture_path;
 
-	char		*n_xpm;              // North texture in XPM format.
-	char		*s_xpm;              // South texture in XPM format.
-	char		*e_xpm;              // East texture in XPM format.
-	char		*w_xpm;              // West texture in XPM format.
-	void		*tile;	//Pointer to XPM file
-	void		*tree;	//Pointer to XPM file
-	void		*north_char;	//Pointer to XPM file
-	void		*south_char;	//Pointer to XPM file
-	void		*east_char;	//Pointer to XPM file
-	void		*west_char;	//Pointer to XPM file
+	char		*n_xpm;
+	char		*s_xpm;
+	char		*e_xpm;
+	char		*w_xpm;
+	void		*tile;
+	void		*tree;
+	void		*north_char;
+	void		*south_char;
+	void		*east_char;
+	void		*west_char;
 
-	double		player_view;         // Player's field of view angle.
+	double		player_view;
 
-	double		x_player;            // Player's x-coordinate.
-	double		y_player;            // Player's y-coordinate.
+	double		x_player;
+	double		y_player;
 
-	double		x_step;              // Step size in the x-direction.
-	double		y_step;              // Step size in the y-direction.
+	double		x_step;
+	double		y_step;
 
-	int			x_m;                 // Map index in the x-direction.
-	int			y_m;                 // Map index in the y-direction.
+	int			x_m;
+	int			y_m;
 
-	char		player_direction;    // Direction the player is facing.
-	int		up;//Move Up trigger
-	int		down;//Move Down trigger
-	int		left;//Move Left trigger
-	int		right;//Move Right trigger
-	int		rot_l;//Rotate Left trigger
-	int		rot_r;//Rotate Right trigger
+	char		player_direction;
+	int			up;
+	int			down;
+	int			left;
+	int			right;
+	int			rot_l;
+	int			rot_r;
 
+	double		ray_distance;
+	double		corrected_distance;
 
+	int			ceiling_color;
+	int			floor_color;
 
-	double		ray_distance;        // Distance to the intersection point.
-	double		corrected_distance;  // Corrected distance to handle fisheye effect.
+	double		ray_position;
 
-	int			ceiling_color;       // Color of the ceiling.
-	int			floor_color;         // Color of the floor.
+	double		distance_to_projection;
+	double		projection_3d;
+	double		floor_ceiling_origin;
+	char		*color;
 
-	double		ray_position;        // Position of the ray in the view plane.
-
-	double		distance_to_projection; // Distance from player to the projection plane.
-	double		projection_3d;       // Height of the wall projected onto the screen.
-	double		floor_ceiling_origin; // Origin position for floor and ceiling rendering.
-	char		*color;              // Temporary color storage.
-
-	t_parsing	*parsing;            // Pointer to parsing data.
-	t_texture_data	display_data;      // Texture data for rendering.
+	t_parsing	*parsing;
+	t_texture_data	display_data;
 }	t_mlx;
 
 // Check if the file ends with .cub.
 void	check_file_extension(char **argv);
 // Check if the program is passed a valid file argument.
-int	validate_arguments(char **argv, int argc);
+int		validate_arguments(char **argv, int argc);
 // Get the number of lines in a file given its file descriptor.
-int	count_file_lines(int fd);
+int		count_file_lines(int fd);
 // Main parsing function to parse the input file.
 void	parse_cub_file(int argc, char **argv, t_parsing *parsing);
 // Check each line of the map for valid characters and player position.
@@ -176,11 +184,11 @@ void	validate_line_content(t_parsing *parsing, int index, int *player_index);
 // Check if the map has a valid player position.
 void	validate_map_and_player(t_parsing *parsing);
 // Helper function to extract RGB color components.
-int	update_color_components(char s, int *vergul, int *nbr, int *rgb);
+int		update_color_components(char s, int *vergul, int *nbr, int *rgb);
 // Check if the line contains valid color values.
-int	validate_color(char *line);
+int		validate_color(char *line);
 // Extract the color value from a line.
-int	get_color(char *line, char *str);
+int		get_color(char *line, char *str);
 // Get color values from the parsing.
 void	color_values(t_parsing *parsing, char *line, t_index *index);
 // Error handling for parsing errors.
@@ -195,45 +203,57 @@ void	initialize_parsing(t_parsing *parsing);
 void	initialize_index(t_index *number);
 // Assign the parsed line to the map.
 void	process_map_line(t_parsing *parsing, t_index *index, char *line);
- // Extract map info from the input file.
-void	get_info_map(t_parsing *parsing, char *line, t_index *index, int *n_tex);
+// Extract map info from the input file.
+void	get_info_map(t_parsing *parsing, char *line, t_index \
+	*index, int *n_tex);
 // Read the map from the input file.
 void	read_map(char *file, t_parsing *parsing);
 // Check the surroundings of player characters.
 void	is_invalid_player_position(t_parsing *parsing, int i, int index);
 // Check the surroundings of '0' characters.
-int	is_invalid_zero_position(t_parsing *parsing, int index, char *line, int i);
+int		is_invalid_zero_position(t_parsing *parsing, int index, \
+	char *line, int i);
 // Get and check the texture path.
 char	*get_path_check(char *line, char *ser);
 // Assign texture paths to the parsing structure.
-void	assign_texture_path(t_parsing *parsing, char *line, int *number , t_index *index);
+void	assign_texture_path(t_parsing *parsing, char *line, int *number \
+	, t_index *index);
 // Check if the line contains a valid texture specifier.
-int	is_texture_line(char *line );
+int		is_texture_line(char *line );
 // Check if a character is a valid map character.
-int	is_invalid_map_character(char c);
+int		is_invalid_map_character(char c);
 // Check if the line contains '0' characters.
-int	contains_zero_character(char *str);
-// Loads XPM images for rendering and stores their data in the provided window structure.
+int		contains_zero_character(char *str);
+/* Loads XPM images for rendering and stores their data
+	in the provided window structure.*/
 void	images_to_xpm(t_mlx *window);
 // Find the initial player position.
 void	get_player_position(t_mlx *window);
 // Calculate the distance between player and a wall.
-double	distance_view(double y_player, double x_player, double y_wall, double x_wall);
+double	distance_view(double y_player, double x_player, \
+	double y_wall, double x_wall);
 // Correct the distance based on fisheye effect.
 double	correct_distance(t_mlx *mlx, double angle);
-//Sets the color of a pixel at the specified coordinates in the image data structure.
+/* Sets the color of a pixel at the specified
+coordinates in the image data structure. */
 void	set_pixel_color(t_texture_data *data, int x, int y, int color);
 // Set the player's direction based on the map coordinates.
 char	set_directions(double y_coordinate, double x_coordinate, t_mlx *window);
-// Renders a 3D view of the scene based on the provided distance and player direction.
-void	render_3d_view(double distance, int height, t_mlx *mlx, char player_diretion);
-// Retrieves the color data for the north-facing wall texture based on the given index.
+/* Renders a 3D view of the scene based on the
+provided distance and player direction. */
+void	render_3d_view(double distance, int height, t_mlx *mlx, \
+	char player_diretion);
+/* Retrieves the color data for the north-facing wall
+ texture based on the given index.*/
 char	*north_texture(t_mlx *mlx, int i);
-// Retrieves the color data for the south-facing wall texture based on the given index.
+/* Retrieves the color data for the south-facing wall
+ texture based on the given index.*/
 char	*south_texture(t_mlx *mlx, int i);
-// Retrieves the color data for the east-facing wall texture based on the given index.
+/* Retrieves the color data for the east-facing wall
+ texture based on the given index.*/
 char	*east_texture(t_mlx *mlx, int i);
-// Retrieves the color data for the west-facing wall texture based on the given index.
+/* Retrieves the color data for the west-facing wall
+ texture based on the given index.*/
 char	*west_texture(t_mlx *mlx, int i);
 // Performing the ray casting process to calculate rendering information.
 void	calculate_ray_intersections(t_mlx *window, double angle, int x);
@@ -251,19 +271,20 @@ void	move_player(t_mlx *window);
 void	rotate_left(t_mlx *window);
 void	rotate_right(t_mlx *window);
 // Handles keyboard input and triggers corresponding actions.
-int	release_keys(int press, t_mlx *window);
+int		release_keys(int press, t_mlx *window);
 // Handles keyboard input and triggers corresponding actions.
-int	get_keys(int press, t_mlx *window);
+int		get_keys(int press, t_mlx *window);
 // Print minimap to the screen
 void	put_minimap(t_mlx *window);
-int	game_loop(t_mlx *window);
+int		game_loop(t_mlx *window);
 void	hooks(t_mlx *window);
-int	destroy_window(t_mlx *window);
-int	is_wall_detector(t_mlx *window, int minus, double y_step, double x_step);
+int		destroy_window(t_mlx *window);
+int		is_wall_detector(t_mlx *window, int minus, double y_step \
+	, double x_step);
 
+/* Testing */
 
-//testers
-void print_parsing_info(t_parsing *parsing);
-void print_index_info(t_index *index);
-void print_map(t_parsing *parsing);
+void	print_parsing_info(t_parsing *parsing);
+void	print_index_info(t_index *index);
+void	print_map(t_parsing *parsing);
 #endif
